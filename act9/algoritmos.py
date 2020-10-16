07cd837377b4d9f1543e7a2f6cad8b4afaf6ee6d
Eduardo Sanchez <eduardo.sanchez4559@alumnos.udg.mx>
2020-10-16 15:47:21 -0500

    Se intenta la interfas de los 2 archivos

---
+++ act9/algoritmos.py
@@ -0,0 +1,9 @@
+from libro import Particula
+
+import math
+
+class distancia_ecualidiana:
+    def distancia_ecualidiana(x1, y1, x2, y2):
+        self.__distancia = math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1))
+
+print(distancia)
--- act9/libro.py
+++ act9/libro.py
@@ -1,3 +1,5 @@
+from algoritmos import distancia_ecualidiana
+
 class Particula:
     def __init__(self, id=0, origen_x=0, origen_y=0, destino_x=0, destino_y=0, velocidad=0, red=0, green=0, blue=0, distancia=0):
         self.__id = id
@@ -9,7 +11,7 @@ class Particula:
         self.__red = red
         self.__green = green
         self.__blue = blue
-        self.__distancia = distancia
+        self.__distancia = distancia_ecualidiana(origen_x=0, origen_y=0, destino_x=0, destino_y=0)
 
     def __str__(self):
         return (
