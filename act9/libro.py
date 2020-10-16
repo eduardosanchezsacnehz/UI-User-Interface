dde4bdd51122a0bb5775e940c225e8d3d6473cce
Eduardo Sanchez <eduardo.sanchez4559@alumnos.udg.mx>
2020-10-16 12:54:04 -0500

    Se crea la clase libro

---
+++ act9/libro.py
@@ -0,0 +1,12 @@
+class libro
+    def __init__(self, id="", origen_x=0, origen_y=0, destino_x=0, destino_y=0, velocidad=0, red=0, green=0, blue=0, distancia=0):
+        self.__id = id
+        self.__origen_x = origen_x
+        self.__origen_y = origen_y
+        self.__destino_x = destino_x
+        self.__destino_y = destino_y
+        self.__velocidad = velocidad
+        self.__red = red
+        self.__green = green
+        self.__blue = blue
+        self.__distancia = distancia
