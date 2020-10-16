6bf675925a47e329520acd0ce0f0fde03814a6c1
Eduardo Sanchez <eduardo.sanchez4559@alumnos.udg.mx>
2020-10-16 14:44:17 -0500

    Un problema se soluciona... y otro nuevo inicia

--- act9/libreria.py
+++ act9/libreria.py
@@ -14,9 +14,9 @@ class Libreria:
         for libro in self__libros:
             print(libro)
 
-    l01 = Particula(id=1234567890, origen_x=12, origen_y=12, destino_x=0, destino_y=0, velocidad=10, red=0, green=0, blue=0, distancia=100.15)
-    l02 = Particula("Python", "Guiado")
-    libreria = Libreria()
-    libreria.agregar_final(l01)
-    libreria.agregar_inicio(l02)
-    libreria.mostrar()
+l01 = Particula(id=1234567890, origen_x=12, origen_y=12, destino_x=0, destino_y=0, velocidad=10, red=0, green=0, blue=0, distancia=100.15)
+l02 = Particula("Python", "Guiado")
+libreria = Libreria()
+libreria.agregar_final(l01)
+libreria.agregar_inicio(l02)
+libreria.mostrar()
