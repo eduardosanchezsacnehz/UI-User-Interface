38169cee83d70dddd29c5ef653571bf6c3c69220
Eduardo Sanchez <eduardo.sanchez4559@alumnos.udg.mx>
2020-10-16 14:48:30 -0500

    YA QUEDÓgit add libreria.py ! nota personal: revisa que el progrma tenga los puntos y guiones que necesita

--- act9/libreria.py
+++ act9/libreria.py
@@ -11,7 +11,7 @@ class Libreria:
         self.__libros.insert(0, libro)
 
     def mostrar(self):
-        for libro in self__libros:
+        for libro in self.__libros:
             print(libro)
 
 l01 = Particula(id=1234567890, origen_x=12, origen_y=12, destino_x=0, destino_y=0, velocidad=10, red=0, green=0, blue=0, distancia=100.15)
