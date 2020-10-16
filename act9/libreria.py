4259fa26a072af8b9d178333dfc2f0877837ea8d
Eduardo Sanchez <eduardo.sanchez4559@alumnos.udg.mx>
2020-10-16 14:09:49 -0500

    Marca error al crear clase y no se por que insertar carita triste

---
+++ act9/libreria.py
@@ -0,0 +1,22 @@
+from libro import Particula
+
+class Libreria:
+    def __init__(self):
+        self.__libros = []
+
+    def agregar_final(self, libro:Particula):
+        self.__libros.append(libro)
+
+    def agregar_inicio(self, libro:Particula):
+        self.__libros.insert(0, libro)
+
+    def mostrar(self):
+        for libro in self__libros:
+            print(libro)
+
+    l01 = Particula(id=1234567890, origen_x=12, origen_y=12, destino_x=0, destino_y=0, velocidad=10, red=0, green=0, blue=0, distancia=100.15)
+    l02 = Particula("Python", "Guiado")
+    libreria = Libreria()
+    libreria.agregar_final(l01)
+    libreria.agregar_inicio(l02)
+    libreria.mostrar()
