52a2b5b7009cedc3437be5477c9c659182d027ea
Eduardo Sanchez <eduardo.sanchez4559@alumnos.udg.mx>
2020-10-16 13:23:04 -0500

    Se muestan los valores

--- act9/libro.py
+++ act9/libro.py
@@ -1,5 +1,5 @@
-class libro
-    def __init__(self, id="", origen_x=0, origen_y=0, destino_x=0, destino_y=0, velocidad=0, red=0, green=0, blue=0, distancia=0):
+class Particula:
+    def __init__(self, id=0, origen_x=0, origen_y=0, destino_x=0, destino_y=0, velocidad=0, red=0, green=0, blue=0, distancia=0):
         self.__id = id
         self.__origen_x = origen_x
         self.__origen_y = origen_y
@@ -10,3 +10,20 @@ class libro
         self.__green = green
         self.__blue = blue
         self.__distancia = distancia
+
+    def __str__(self):
+        return (
+            'id: ' + str(self.__id) + '\n' +
+            'origen_x: ' + str(self.__origen_x) + '\n' +
+            'origen_y: ' + str(self.__origen_y) + '\n' +
+            'destino_x: ' + str(self.__destino_x) + '\n' +
+            'destino_y: ' + str(self.__destino_y) + '\n' +
+            'velocidad: ' + str(self.__velocidad) + '\n' +
+            'red: ' + str(self.__red) + '\n' +
+            'green: ' + str(self.__green) + '\n' +
+            'blue: ' + str(self.__blue) + '\n' +
+            'distncia: ' + str(self.__distancia) + '\n' 
+        )
+
+l01 = Particula(id=1234567890, origen_x=12, origen_y=12, destino_x=0, destino_y=0, velocidad=10, red=0, green=0, blue=0, distancia=100.15)
+print(l01)
