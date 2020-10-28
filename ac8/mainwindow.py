783556b62e103b3b15e7abfe02f2483aa73d6fcc
Eduardo Sanchez <eduardo.sanchez4559@alumnos.udg.mx>
2020-10-28 12:28:37 -0600

    Ya imprime salida

--- ac8/mainwindow.py
+++ ac8/mainwindow.py
@@ -15,6 +15,32 @@ class MainWindow(QMainWindow):
         self.ui.mostrar_pushButton.clicked.connect(self.click_mostrar)
 
     @Slot()
+    def click_mostrar(self):
+        #self.administrador.mostrar()
+        self.ui.salida.clear()
+        self.ui.salida.insertPlainText(str(self.administrador))
+
+    @Slot()
+    def click_agregar(self):
+        id = self.ui.id_lineEdit.text()
+        origen_x = self.ui.origen_x_spinBox.value()
+        origen_y = self.ui.origen_y_spinBox.value()
+        destino_x = self.ui.destino_x_spinBox.value()
+        destino_y = self.ui.destino_y_spinBox.value()
+        velocidad = self.ui.velocidad_lineEdit.text()
+        red = self.ui.red_spinBox.value()
+        green = self.ui.green_spinBox.value()
+        blue = self.ui.blue_spinBox.value()
+
+        particula = Particula(id, origen_x, origen_y, destino_x, destino_y, velocidad, red, green, blue)
+        self.administrador.agregar_final(particula)
+
+
+        #print (id, origen_x, origen_y, destino_x, destino_y, velocidad, red, green, blue)
+        #self.ui.salida.insertPlainText (id + str(origen_x) + str(origen_y) + str(destino_x) + str(destino_y) + 
+        #velocidad + str(red) + str(green) + str(blue))
+
+    @Slot()
     def click_agregar_inicio(self):
         id = self.ui.id_lineEdit.text()
         origen_x = self.ui.origen_x_spinBox.value()
@@ -28,19 +54,3 @@ class MainWindow(QMainWindow):
 
         particula = Particula(id, origen_x, origen_y, destino_x, destino_y, velocidad, red, green, blue)
         self.administrador.agregar_inicio(particula)
-
-        @Slot()
-        def click_agregar(self):
-            id = self.ui.id_lineEdit.text()
-            origen_x = self.ui.origen_x_spinBox.value()
-            origen_y = self.ui.origen_y_spinBox.value()
-            destino_x = self.ui.destino_x_spinBox.value()
-            destino_y = self.ui.destino_y_spinBox.value()
-            velocidad = self.ui.velocidad_lineEdit.text()
-            red = self.ui.red_spinBox.value()
-            green = self.ui.green_spinBox.value()
-            blue = self.ui.blue_spinBox.value()
-
-        #print (id, origen_x, origen_y, destino_x, destino_y, velocidad, red, green, blue)
-        #self.ui.salida.insertPlainText (id + str(origen_x) + str(origen_y) + str(destino_x) + str(destino_y) + 
-        #velocidad + str(red) + str(green) + str(blue))
