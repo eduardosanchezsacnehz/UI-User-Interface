0c1b5e2d6f898957d3a37d72be6201da57abae88
Eduardo Sanchez <eduardo.sanchez4559@alumnos.udg.mx>
2020-10-28 12:19:16 -0600

    desalinié el texto por error

--- ac8/mainwindow.py
+++ ac8/mainwindow.py
@@ -7,20 +7,25 @@ from Libreria_Part.particula import Particula
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
+        self.administrador = Administrador()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-
         self.ui.agregar_final_pushButton.clicked.connect(self.click_agregar)
+        self.ui.agregar_inicio_pushButton.clicked.connect(self.click_agregar_inicio)
+        self.ui.mostrar_pushButton.clicked.connect(self.click_mostrar)
 
-    @Slot()
-    def click_agregar(self):
-        id = self.ui.id_lineEdit.text()
-        origen_x = self.ui.origen_x_spinBox.value()
-        origen_y = self.ui.origen_y_spinBox.value()
-        destino_x = self.ui.destino_x_spinBox.value()
-        destino_y = self.ui.destino_y_spinBox.value()
-        velocidad = self.ui.velocidad_lineEdit.text()
-        red = self.ui.red_spinBox.value()
-        green = self.ui.green_spinBox.value()
-        blue = self.ui.blue_spinBox.value()
+        @Slot()
+        def click_agregar(self):
+            id = self.ui.id_lineEdit.text()
+            origen_x = self.ui.origen_x_spinBox.value()
+            origen_y = self.ui.origen_y_spinBox.value()
+            destino_x = self.ui.destino_x_spinBox.value()
+            destino_y = self.ui.destino_y_spinBox.value()
+            velocidad = self.ui.velocidad_lineEdit.text()
+            red = self.ui.red_spinBox.value()
+            green = self.ui.green_spinBox.value()
+            blue = self.ui.blue_spinBox.value()
 
+        #print (id, origen_x, origen_y, destino_x, destino_y, velocidad, red, green, blue)
+        #self.ui.salida.insertPlainText (id + str(origen_x) + str(origen_y) + str(destino_x) + str(destino_y) + 
+        #velocidad + str(red) + str(green) + str(blue))
