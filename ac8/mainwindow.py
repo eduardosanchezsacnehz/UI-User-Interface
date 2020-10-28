b52f25803e9968acb84810037da3081ed572dcb8
Eduardo Sanchez <eduardo.sanchez4559@alumnos.udg.mx>
2020-10-28 11:51:30 -0600

    Se implementan los datos de origen destino etc

--- ac8/mainwindow.py
+++ ac8/mainwindow.py
@@ -5,11 +5,19 @@ from ui_mainwindows import Ui_MainWindow
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
-        ui = Ui_MainWindow()
-        ui.setupUi(self)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
 
-        ui.agrega_final_pushButton.clicked.connect(self.click_agregar)
+        self.ui.agrega_final_pushButton.clicked.connect(self.click_agregar)
 
     @Slot()
     def click_agregar(self):
-        print('click')
+        id = self.ui.id_lineEdit.text()
+        origen_x = self.ui.origen_x_spinBox.value()
+        origen_y = self.ui.origen_y_spinBox.value()
+        destino_x = self.ui.destino_x_spinBox.value()
+        destino_y = self.ui.destino_y_spinBox.value()
+        velocidad = self.ui.velocidad_lineEdit.text()
+        red = self.ui.red_spinBox.value()
+        green = self.ui.green_spinBox.value()
+        blue = self.ui.blue_spinBox.value()
