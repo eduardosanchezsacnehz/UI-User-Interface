7308ad8a5c6d2b7041c7adf3e02f5cb16e7c864f
Eduardo Sanchez <eduardo.sanchez4559@alumnos.udg.mx>
2020-10-28 12:20:31 -0600

    agregar inicio

--- ac8/mainwindow.py
+++ ac8/mainwindow.py
@@ -14,6 +14,21 @@ class MainWindow(QMainWindow):
         self.ui.agregar_inicio_pushButton.clicked.connect(self.click_agregar_inicio)
         self.ui.mostrar_pushButton.clicked.connect(self.click_mostrar)
 
+    @Slot()
+    def click_agregar_inicio(self):
+        id = self.ui.id_lineEdit.text()
+        origen_x = self.ui.origen_x_spinBox.value()
+        origen_y = self.ui.origen_y_spinBox.value()
+        destino_x = self.ui.destino_x_spinBox.value()
+        destino_y = self.ui.destino_y_spinBox.value()
+        velocidad = self.ui.velocidad_lineEdit.text()
+        red = self.ui.red_spinBox.value()
+        green = self.ui.green_spinBox.value()
+        blue = self.ui.blue_spinBox.value()
+
+        particula = Particula(id, origen_x, origen_y, destino_x, destino_y, velocidad, red, green, blue)
+        self.administrador.agregar_inicio(particula)
+
         @Slot()
         def click_agregar(self):
             id = self.ui.id_lineEdit.text()
