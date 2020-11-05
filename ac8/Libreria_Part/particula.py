cfe801c9d800482168b0682d65fed1cfe72ba23e
Eduardo Sanchez <eduardo.sanchez4559@alumnos.udg.mx>
2020-11-04 23:23:40 -0600

    Termino el def to dict

--- ac8/Libreria_Part/particula.py
+++ ac8/Libreria_Part/particula.py
@@ -1,7 +1,7 @@
-from algoritmos import distancia_euclidiana
+from .algoritmos import distancia_euclidiana
 
 class Particula:
-    def __init__(self,id="", origen_x=0, origen_y=0, destino_x=0,  destino_y=0, velocidad=0, red=0, green=0, blue=0, distancia="0"):
+    def __init__(self, id=0, origen_x=0, origen_y=0, destino_x=0,  destino_y=0, velocidad=0, red=0, green=0, blue=0, distancia=0):
         self.__id = id
         self.__origen_x = origen_x
         self.__origen_y = origen_y
@@ -15,14 +15,27 @@ class Particula:
 
     def __str__(self):
         return (
-            'Id:  ' + str(self.__id) + '\n' +
-            'Origen x:  ' + str(self.__origen_x) + '\n' +
-            'Origen y:  ' + str(self.__origen_y) + '\n' +
-            'Destino x:  ' + str(self.__destino_x) + '\n' +
-            'Destino y:  ' + str(self.__destino_y) + '\n' +
-            'Velocidad:  ' + str(self.__velocidad) + '\n' +
-            'Red:  ' + str(self.__red) + '\n' +
-            'Green:  ' + str(self.__green) + '\n' +
-            'Blue:  ' + str(self.__blue) + '\n' +
-            'Distancia:  ' + str(self.__distancia) + '\n'
+            'id:  ' + str(self.__id) + '\n' +
+            'origen_x:  ' + str(self.__origen_x) + '\n' +
+            'origen_y:  ' + str(self.__origen_y) + '\n' +
+            'destino_x:  ' + str(self.__destino_x) + '\n' +
+            'destino_y:  ' + str(self.__destino_y) + '\n' +
+            'velocidad:  ' + str(self.__velocidad) + '\n' +
+            'red:  ' + str(self.__red) + '\n' +
+            'green:  ' + str(self.__green) + '\n' +
+            'blue:  ' + str(self.__blue) + '\n' +
+            'distancia:  ' + str(self.__distancia) + '\n'
         )
+
+    def to_dict(self):
+        return {
+            "id": self.__id,
+            "origen_x": self.__origen_x,
+            "origen_y": self.__origen_y,
+            "destino_x": self.__destino_x,
+            "destino_y": self.__destino_y,
+            "velocidad": self.__velocidad,
+            "red": self.__red,
+            "green": self.__green,
+            "blue": self.__blue
+        }
